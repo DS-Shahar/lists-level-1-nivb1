58b21708b6c3374dbc180adebe71caf0f58dd0c7
nivb1 <nivbarzilay2103@gmail.com>
2024-12-02 15:37:04 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,30 @@
-class Main {
-  public static Node<Integer> q1(int x, int y) {
-		Node<Integer> a = new Node<Integer>((int)(Math.random()*(x+y)+x));
+package niv;
+
+import java.util.Scanner;
+
+public class Main {
+	public static Scanner reader = new Scanner(System.in);
+	public static Node<Integer> a(int[] a){
+		Node<Integer> fr = new Node<Integer>(a[0]);
+		Node<Integer> last = fr;
+		for(int i=1;i<a.length;i++) {
+			Node<Integer> x = new Node<Integer>(a[i]);
+			last.setNext(x);
+			last=x;
+		}
+		return fr;
+	}
+	public static Node<Integer> buildNode(int x, int y,int n) {
+		Node<Integer> a = new Node<Integer>((int)(Math.random()*(x+y-1)+x));
 		Node<Integer> last = a;
-		for(int i=0;i<19;i++) {
-			Node<Integer> n = new Node<Integer>((int)(Math.random()*(x+y)+x));
-			last.setNext(n);
-			last = n;
+		for(int i=0;i<n-1;i++) {
+			Node<Integer> k = new Node<Integer>((int)(Math.random()*(x+y-1)+x));
+			last.setNext(k);
+			last = k;
 		}
 		return a;
 	}
-	public static int q2(Node<Integer> h, int x) {
+	public static int count(Node<Integer> h, int x) {
 		int count=0;
 		while(h!=null) {
 			if(x==h.getValue())
@@ -18,9 +33,80 @@ class Main {
 		}
 		return count;
 	}
-  public static void main(String[] args) {
-    Node<Integer> a = q1(0,50);
-		System.out.println(a);
-		System.out.println(q2(a,40));
-  }
+	public static void printList(Node<Integer> a) {
+		while(a.getNext()!=null) {
+			System.out.println(a.getValue());
+			a = a.getNext();
+		}
+	}
+	public static void printListRec(Node<Integer> a) {
+		if(a==null)
+			return;
+		System.out.println(a.getValue());
+		printListRec(a.getNext());
+	}
+	public static void printListRecRev(Node<Integer> a) {
+		if(a==null)
+			return;
+		printListRecRev(a.getNext());
+		System.out.println(a.getValue());
+		
+	}
+	public static Node<Integer> inputList(){
+		System.out.println("enter num or -1 to stop");
+		Node<Integer> first = new Node<Integer>(null);
+		Node<Integer> p = first;
+		int num = reader.nextInt();
+		Node<Integer> x;
+		while(num!=-1) {
+			x = new Node<Integer>(num);
+			p.setNext(x);
+			p=x;
+			System.out.println("enter num or -1 to stop");
+			num = reader.nextInt();
+		}
+		return first.getNext();
+	}
+	public static void printZogi(Node<Integer> a) {
+		while(a!=null) {
+			if(a.getValue()%2==0)
+				System.out.println(a.getValue());
+			a=a.getNext();
+		}
+	}
+	public static boolean check(Node<Integer> h, int x) {
+		while(h!=null) {
+			if(x==h.getValue())
+				return true;
+			h = h.getNext();
+		}
+		return false;
+	}
+	public static boolean checkRec(Node<Integer> h, int x) {
+		if(h==null) {
+			return false;
+		}
+		if(h.getValue()==x)
+			return true;
+		return checkRec(h.getNext(),x);
+			
+	}
+	public static void main(String[] args) {
+		int[] arr = {1,2,3,4};
+		System.out.println(a(arr));
+		Node<Integer> rachel = buildNode(1,10,20);
+		System.out.println(rachel);
+		System.out.println("count: "+count(rachel,3));
+		System.out.println();
+//		printList(rachel);
+		System.out.println();
+//		printListRecRev(rachel);
+		printListRec(rachel);
+//		Node<Integer> jj = inputList();
+//		System.out.println(jj);
+		System.out.println("zogi:");
+		printZogi(rachel);
+		System.out.println(checkRec(rachel,1));
+	}
+
 }
