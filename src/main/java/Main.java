f261b4154cc0ea08d38fea93bb85d7f164a5b670
nivb1 <nivbarzilay2103@gmail.com>
2024-12-09 16:22:42 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -101,6 +101,107 @@ public class Main {
 		return checkRec(h.getNext(),x);
 			
 	}
+	public static Node<Integer> buildNode(int x, int y,int n) {
+		//from class
+		Node<Integer> a = new Node<Integer>((int)(Math.random()*(x+y-1)+x));
+		Node<Integer> last = a;
+		for(int i=0;i<n-1;i++) {
+			Node<Integer> k = new Node<Integer>((int)(Math.random()*(x+y-1)+x));
+			last.setNext(k);
+			last = k;
+		}
+		return a;
+	}
+	public static Node<Integer> earse(Node<Integer> a,int n){
+		//q6
+		if(a.getValue()==n)
+			return a.getNext();
+		Node<Integer> list = a;
+		Node<Integer> save= list;
+		while(a != null) {
+			if(a.getValue()==n) {
+				list.setNext(a.getNext());
+				return save;
+			}
+			list = a;
+			a=a.getNext();
+		}
+		return save;
+	}
+	public static Node<Integer> earseindex(Node<Integer> a, int i){
+		if(i==1) {
+			return a.getNext();
+		}
+		Node<Integer> prev = a;
+		Node<Integer> save = a;
+		for(int j=0;j<i-1;j++) {
+			prev=a;
+			a=a.getNext();
+		}
+		prev.setNext(a.getNext());
+		return save;
+		
+	}
+	public static boolean isAppear(Node<Integer> L1,Node<Integer> L2,Node<Integer> s) {
+		//q8
+		if(L1==null)
+			return true;
+		if(L2==null)
+			return false;
+		if(L1.getValue()==L2.getValue()) {
+			return isAppear(L1.getNext(),s,s);
+		}
+
+		return isAppear(L1.getNext(),L2.getNext(),s);
+		
+	}
+	public static void printappboth(Node<Integer> L1,Node<Integer> L2) {
+		Node<Integer> saveL2 = L2;
+		while(L1!=null) {
+			
+			L2=saveL2;
+			while(L2!=null) {
+				if(L1.getValue()==L2.getValue())
+					System.out.println(L1.getValue());
+				L2=L2.getNext();
+			}
+			L1=L1.getNext();
+		}
+	}
+	public static Node<Integer> q10(Node<Integer> L1,Node<Integer> L2){
+		Node<Integer> fir = new Node<Integer>(null);
+		Node<Integer> d = fir;
+		Node<Integer> saveL2 = L2;
+		while(L1!=null) {
+			
+			L2=saveL2;
+			while(L2!=null) {
+				if(L1.getValue()==L2.getValue()) {
+					Node<Integer> a = new Node<Integer>(L1.getValue());
+					d.setNext(a);
+					d=a;
+				}
+				L2=L2.getNext();
+			}
+			L1=L1.getNext();
+		}
+		return fir.getNext();
+	}
+	public static Node<Integer> q11(Node<Integer> L1, Node<Integer> L2){
+		Node<Integer> saveL1 = L1;
+		Node<Integer> saveL2 = L2;
+		while(L1!=null) {
+			
+			L2=saveL2;
+			while(L2!=null) {
+				if(L1.getNext().getValue()==L2.getNext().getValue())
+					L1.setNext(L1.getNext().getNext());
+				L2=L2.getNext();
+			}
+			L1=L1.getNext();
+		}
+		return saveL1;
+	}
 	public static void main(String[] args) {
 		int[] arr = {1,2,3,4};
 		System.out.println(a(arr));
